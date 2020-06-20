ed4ffda25e38c4efb3ef194980c7443182bccd92
ayush6624 <ayushg1214@gmail.com>
2020-06-20 23:24:26 +0530

    feat: disconnect on exiting

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import { Test } from './speed.model';
 import { Data, convertToSpeed } from './util';
+import { mongoose } from './db';
 const exec = require('child_process').exec;
 
 let result: string = '';
@@ -14,15 +15,16 @@ child.stdout.on('data', function (data: any) {
 
 child.on('close', function () {
   console.log('closed!!');
-  helperFunction(result);
+  helperFunction(result).then((success) => {
+    success ? console.log('Done') : console.log('failed!');
+  });
 });
 
 const helperFunction = async (result: any) => {
   const data: Data = JSON.parse(result);
   if (data.error) {
     console.log('Error ' + data.error);
-
-    return;
+    return false;
   }
   data.download.speed = convertToSpeed(data.download.bandwidth);
   data.upload.speed = convertToSpeed(data.upload.bandwidth);
@@ -32,4 +34,6 @@ const helperFunction = async (result: any) => {
   } catch (err) {
     console.log('Error => ', err);
   }
+  await mongoose.disconnect();
+  return true;
 };
